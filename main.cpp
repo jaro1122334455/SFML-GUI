1d242c2cb7c5bb3da33c35971442f752546ea2e7
jaro1122334455 <jaro1122334455@gmail.com>
2023-09-25 19:17:44 +0200

    usunięcie niepotrzebnych komentarzy

--- main.cpp
+++ main.cpp
@@ -18,59 +18,10 @@ int main()
     Button b1("START", 50, {200, 70}, {setup::W / 2, setup::H / 2}, sf::Color(77, 144, 120), sf::Color(247, 129, 84));
     Button b2("EXIT", 50, {200, 70}, {setup::W / 2, setup::H / 2 + 100}, sf::Color(77, 144, 120), sf::Color(247, 129, 84));
 
-
-    Button* b3 = new Button("TEST", 50, {200, 70}, {setup::W / 2, setup::H / 2 + 200}, sf::Color(77, 144, 120), sf::Color(247, 129, 84));
-
-    delete b3;
-
-    b3 = nullptr;
-
     Logo logo("SNAKE");
 
-    // Dialog dialog_2 = nullptr;
 
     Dialog* dialog = nullptr;
-
-    // Dialog* dialog_test = new Dialog();
-
-    // delete dialog;
-
-    // dialog = nullptr;
-
-
-
-
-
-
-
-
-
-
-    // ----------------------------------------------------------
-
-    sf::RectangleShape* rect_1 = new sf::RectangleShape();
-
-    rect_1->setSize({200, 200});
-    rect_1->setPosition({setup::W / 2, setup::H / 2});
-    rect_1->setFillColor(sf::Color::Blue);
-
-    // delete rect_1;
-
-    // rect_1 = nullptr;
-
-    // ------------------------------------------------------------
-
-    // delete dialog;
-
-    // dialog = nullptr;
-
-    
-
-
-    // sf::Text logo = Logo("SNAKE");
-
-
-
 
 
     while (window.isOpen())
@@ -89,12 +40,9 @@ int main()
 
                 if(dialog == nullptr)
                 {
-                //     // if((*dialog).isMouseOver(window) == false)
-                //     // {
-                        b1.swapColors(window);
-                        b2.swapColors(window);
-                        // (*b3).swapColors(window);
-                //     // }
+                    b1.swapColors(window);
+                    b2.swapColors(window);
+
                 }
 
                 if(dialog != nullptr)
@@ -149,68 +97,24 @@ int main()
                 }
 
 
-
-
-                
-
-                // if(b3 != nullptr && (*b3).isMouseOver(window))
-                // {
-                //     delete b3;
-                //     b3 = nullptr;
-                // }
-
             }
 
 
         }
 
-        // std::cout << (*dialog).isMouseOver(window) << std::endl;
-
         window.clear(sf::Color(242, 193, 78));
 
         
-        
-
-        
-        
-        // if(b2.isMouseOver(window))
-        // {
-        
-        // }
-
-        // window.draw(shape);
         window.draw(b1);
         window.draw(b2);
 
-        // if(b3 != nullptr)
-        // {
-        //     window.draw(*b3);
-        // }
-
-        // if(dialog_test != nullptr)
-        // {
-        //     window.draw(*dialog_test);
-        // }
-        // window.draw(b3);
         window.draw(logo);
-        // window.draw((*dialog));
 
 
         if (dialog != nullptr)
         {
-            // std::cout << "jest rozne od nullptr" << std::endl;
             window.draw((*dialog));
         }
-        // else
-        // {
-        //     // window.draw((*rect_1));
-        //     std::cout << "jest rozne od null" << std::endl;
-        // }
-        // else if(dialog == nullptr)
-        // {
-        //     window.draw((*rect_1));
-        // }
-
         window.display();
     }
 
